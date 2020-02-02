9a329953a29e4736b2610e20d8e205eb587b2a35
Adele Gauvrit <gauvrit.adele@gmail.com>
2020-02-02 18:27:41 +0100

    Set maximum height to 200px

--- src/sandbox/getNodeSize.ts
+++ src/sandbox/getNodeSize.ts
@@ -1,9 +1,19 @@
+const MAX_HEIGHT = 200
 export interface Size {
     width: number,
     height: number
 }
-export const getNodeSize = (node: FrameNode) => ({
-        width: node.width,
-        height: node.height
-    })
+export const getNodeSize = (node: FrameNode) => {
+    const { width, height } = node
+    if (height > MAX_HEIGHT) {
+        return {
+            width: Math.floor(MAX_HEIGHT * width / height),
+            height: MAX_HEIGHT
+        }
+    } 
+    return (
+    {
+        width,
+        height
+    })}
 
